7a0b62786fd213d155249b9f26930f31d8aa68e6
anujsinghwd <anujsingh179@yahoo.com>
2017-02-23 23:46:47 +0530

    first commit

---
+++ getdistancetime.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    $addressFrom = 'Input origin Here';
+    $addressTo = 'Input Destination Here';
+    $formattedAddrFrom = str_replace(' ','+',$addressFrom);
+    $formattedAddrTo = str_replace(' ','+',$addressTo);
+
+
+    echo $geocode = file_get_contents('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$formattedAddrFrom.'&destinations='.$formattedAddrTo.'&key=AIzaSyANAWSdDBQGEe7OzoK3WPJtwpQnT6yD9oQ');
+    echo "<br>";
+    $outputTo = json_decode($geocode);
+    print_r($outputTo);
+
+}
+?>
